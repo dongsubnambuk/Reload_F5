e6953e6911e91205928a691543ee910f14949bc0
DonggyunHan <hdg5639@daum.net>
2024-09-28 22:29:34 +0900

    FirstCommit

---
+++ GatewayServer/src/test/java/com/f5/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
