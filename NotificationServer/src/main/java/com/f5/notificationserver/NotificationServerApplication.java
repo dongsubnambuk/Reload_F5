b3c7cf36382dceba37b846c4c97977dba64034e9
DonggyunHan <hdg5639@daum.net>
2024-09-28 22:04:47 +0900

    first commit

---
+++ NotificationServer/src/main/java/com/f5/notificationserver/NotificationServerApplication.java
@@ -0,0 +1,13 @@
+package com.f5.notificationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServerApplication.class, args);
+    }
+
+}
