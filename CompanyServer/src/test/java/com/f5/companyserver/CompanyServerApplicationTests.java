5e9a12a0bf6ea81094773d7e9a972f1956e4216a
DonggyunHan <hdg5639@daum.net>
2024-09-28 22:25:54 +0900

    FirstCommit

---
+++ CompanyServer/src/test/java/com/f5/companyserver/CompanyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f5.companyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
