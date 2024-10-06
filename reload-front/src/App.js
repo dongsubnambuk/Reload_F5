d183823b936db2dbbbcb0ca278bdfc564ca12b55
dongsubnambuk <ehdtjq1278@naver.com>
2024-10-06 15:52:36 +0900

    관리자 & 유저 구분 화면크기 조정

--- reload-front/src/App.js
+++ reload-front/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 
 function App() {
+  const role = 'user'; // 여기서 role이 'user' 또는 'admin'으로 설정됩니다.
+
   return (
-    <div className="mobile-container">
-   모바일 프레임
-  </div>
+    <div className={role === 'user' ? 'mobile-container' : 'web-container'}>
+      {role === 'user' ? '모바일 프레임' : '웹 프레임'}
+    </div>
   );
 }
 
