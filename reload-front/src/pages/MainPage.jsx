c84d19b53a4ef869b679d2c7f17499f099c95f70
dongsubnambuk <ehdtjq1278@naver.com>
2024-10-06 21:57:08 +0900

    검색창 추가 및 설치리스트 파일 추가

--- reload-front/src/pages/MainPage.jsx
+++ reload-front/src/pages/MainPage.jsx
@@ -3,37 +3,53 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css'; // Swiper 기본 CSS
 import 'swiper/css/navigation'; // Navigation 모듈의 CSS
 import 'swiper/css/pagination'; // Pagination 모듈의 CSS
-import '../CSS/MainPage.css';
+import '../CSS/MainPage.css'; // MainPage 관련 CSS
 import { Navigation, Pagination, Autoplay } from 'swiper/modules'; // 모듈을 swiper/modules에서 가져오기
+import { Input } from 'antd'; // Ant Design의 Input 모듈을 가져오기
 
-const MainPage = () =>{
+const { Search } = Input;
 
-    return(
+const MainPage = () => {
+    const onSearch = (value) => console.log(value); // 검색창에서 입력된 값을 처리하는 함수
+
+    return (
         <>
             <div className="swiper-container">
-        <Swiper
-          spaceBetween={50}
-          slidesPerView={1}
-          navigation
-          pagination={{ clickable: true }}
-          autoplay={{ delay: 3000, disableOnInteraction: false }}
-          modules={[Navigation, Pagination, Autoplay]}
-          style={{ height: '23vh' }}
-        >
-          <SwiperSlide className="slide-content">
-            1
-          </SwiperSlide>
-          <SwiperSlide className="slide-content">
-            2
-          </SwiperSlide>
-          <SwiperSlide className="slide-content">
-            3
-          </SwiperSlide>
-          <SwiperSlide className="slide-content">
-            4
-          </SwiperSlide>
-        </Swiper>
-      </div>
+                <Swiper
+                    spaceBetween={50}
+                    slidesPerView={1}
+                    navigation
+                    pagination={{ clickable: true }}
+                    autoplay={{ delay: 3000, disableOnInteraction: false }}
+                    modules={[Navigation, Pagination, Autoplay]}
+                    style={{ height: '23vh' }}
+                >
+                    <SwiperSlide className="slide-content">
+                        1
+                    </SwiperSlide>
+                    <SwiperSlide className="slide-content">
+                        2
+                    </SwiperSlide>
+                    <SwiperSlide className="slide-content">
+                        3
+                    </SwiperSlide>
+                    <SwiperSlide className="slide-content">
+                        4
+                    </SwiperSlide>
+                </Swiper>
+            </div>
+
+         
+            <div className="search-container">
+                <div className="search-box">
+                    <Search
+                        placeholder="검색어를 입력하세요"
+                        enterButton="검색"
+                        size="large"
+                        onSearch={onSearch} // 검색 버튼 클릭 시 실행될 함수
+                    />
+                </div>
+            </div>
         </>
     );
 };
